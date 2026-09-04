d7179157bbc33ca42708454d81a81e024ab918af
LORENZO GNUTZMANN DA SILVA <lorenzo_g_da-silva@estudante.sesisenai.org.br>
2026-09-04 11:03:47 -0300

    medCare v2

--- medCare.sql
+++ medCare.sql
@@ -1,8 +1,14 @@
-select
-	m.nome as medico,
-	m.crm,
-	e.nome as especialidade,
-	m.valor_consulta
-FROM medicos m
-INNER JOIN especialidades e ON m.especialidade_id = e.id
-ORDER BY m.valor_consulta DESC;
+insert into medicos (especialidade_id, nome, crm, valor_consulta) values
+	(1, 'Davi Brito', '101000', 1000000.99),
+	(2, 'Júlio', '777777', 77.70),
+	(3, 'Analcleto', '221221', 69.90);
+
+insert into consultas (medico_id, paciente_id, data_hora, status) values
+	(1, 1, '2026-09-20', 'agendada'),
+	(2, 2, '2026-10-09', 'realizada'),
+	(3, 1, '2026-09-21', 'realizada');
+
+insert into consultas (medico_id, paciente_id, data_hora, status) values
+	(1, 1, '2026-09-22', 'agendada');
+
+
